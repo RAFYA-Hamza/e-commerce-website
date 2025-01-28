ded389b453644fffa1b77881becaf6642897b3ef
RYham <rafyahamza99@gmail.com>
2025-01-28 23:41:44 +0100

    Styling of the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,53 @@
 import logoImg from "../assets/logo.svg";
+import cartImg from "../assets/cart.svg";
+import favoriteImg from "../assets/favorite.svg";
+import profileImg from "../assets/profile.svg";
 
 // const navItem = ["Home", "About", "Contact Us", "Blog"];
 
 export default function Header() {
+  function handleClick() {
+    console.log("clicked");
+  }
   return (
-    <header>
-      <img src={logoImg} alt="My logo" />
-      <input type="text" placeholder="Search" />
+    <header className="px-[10rem] py-[1rem] flex gap-[2rem]">
+      <div>
+        <img src={logoImg} alt="My logo" />
+      </div>
+
+      <div>
+        <input type="text" placeholder="Search" />
+      </div>
 
       <nav>
-        <ul>
+        <ul className="flex">
           <li>
             <a href="">Home</a>
           </li>
           <li>
             <a href="">About</a>
-            <li>
-              <a href="">Contact Us</a>
-            </li>
-            <li>
-              <a href="">Blog</a>
-            </li>
+          </li>
+          <li>
+            <a href="">Contact Us</a>
+          </li>
+          <li>
+            <a href="">Blog</a>
           </li>
         </ul>
       </nav>
 
       <div>
-        <button></button>
+        <button onClick={handleClick}>
+          <img src={favoriteImg} alt="Favorite image" />
+        </button>
 
-        <button></button>
+        <button>
+          <img src={cartImg} alt="Cart image" />
+        </button>
 
-        <button></button>
+        <button>
+          <img src={profileImg} alt="Profile image" />
+        </button>
       </div>
     </header>
   );
