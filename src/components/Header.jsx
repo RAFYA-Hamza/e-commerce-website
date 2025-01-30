5a33342c1a5b99f4430ac10eee314b0e7e9fdb51
RYham <rafyahamza99@gmail.com>
2025-01-30 11:43:46 +0100

    Creation of the SubNav components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,12 @@ import cartImg from "../assets/cart.svg";
 import favoriteImg from "../assets/favorite.svg";
 import profileImg from "../assets/profile.svg";
 import searchImg from "../assets/search.svg";
+import phoneImg from "../assets/phone.svg";
+import comupterImg from "../assets/computer.svg";
+import swatchImg from "../assets/swatch.svg";
+import cameraImg from "../assets/camera.svg";
+import headphoneImg from "../assets/headphone.svg";
+import gamingImg from "../assets/gaming.svg";
 
 import IconButton from "./UI/IconButton";
 
@@ -14,25 +20,25 @@ export default function Header() {
   }
 
   return (
-    <header className="h-[5.5rem] px-[10rem] py-[1rem] flex justify-between items-center gap-2rem">
-      <div>
-        <img src={logoImg} alt="My logo" />
-      </div>
+    <>
+      <header className="h-[5.5rem] px-[10rem] py-[1rem] flex justify-between items-center gap-2rem">
+        <div>
+          <img src={logoImg} alt="My logo" />
+        </div>
 
-      <div className="w-full h-full max-w-[32rem] px-[1rem] py-[1rem] pl-[3rem] relative flex items-center bg-[#F5F5F5] rounded-[0.75rem]">
-        <img
-          className="absolute left-[0.75rem]"
-          src={searchImg}
-          alt="Search icon"
-        />
-        <input
-          className="w-full px-[0.25rem] py-[0.25rem]"
-          type="text"
-          placeholder="Search..."
-        />
-      </div>
+        <div className="w-full h-full max-w-[32rem] px-[1rem] py-[1rem] pl-[3rem] relative flex items-center bg-[#F5F5F5] rounded-[0.75rem]">
+          <img
+            className="absolute left-[0.75rem]"
+            src={searchImg}
+            alt="Search icon"
+          />
+          <input
+            className="w-full px-[0.25rem] py-[0.25rem]"
+            type="text"
+            placeholder="Search..."
+          />
+        </div>
 
-      <nav>
         <ul className="flex justify-between gap-[3.25rem] text-[#656565]">
           <li>
             <a href="#">Home</a>
@@ -47,15 +53,40 @@ export default function Header() {
             <a href="#">Blog</a>
           </li>
         </ul>
+
+        <div className="flex gap-[1rem]">
+          <IconButton url={favoriteImg} descritpion="Favorite image" />
+
+          <IconButton url={cartImg} descritpion="Cart image" />
+
+          <IconButton url={profileImg} descritpion="Profile image" />
+        </div>
+      </header>
+      <nav className="w-full px-[10rem] py-[0.5rem] flex justify-between bg-[#2E2E2E] text-[#797979]">
+        <IconButton url={phoneImg} descritpion="Phones image">
+          Phones
+        </IconButton>
+
+        <IconButton url={comupterImg} descritpion="Computers image">
+          Computers
+        </IconButton>
+
+        <IconButton url={swatchImg} descritpion="Smart Watches image">
+          Smart Watches
+        </IconButton>
+
+        <IconButton url={cameraImg} descritpion="Cameras image">
+          Cameras
+        </IconButton>
+
+        <IconButton url={headphoneImg} descritpion="Headphones image">
+          Headphones
+        </IconButton>
+
+        <IconButton url={gamingImg} descritpion="Gaming image">
+          Gaming
+        </IconButton>
       </nav>
-
-      <div className="flex gap-[1rem]">
-        <IconButton url={favoriteImg} descritpion="Favorite image" />
-
-        <IconButton url={cartImg} descritpion="Cart image" />
-
-        <IconButton url={profileImg} descritpion="Profile image" />
-      </div>
-    </header>
+    </>
   );
 }
--- src/components/UI/IconButton.jsx
+++ src/components/UI/IconButton.jsx
@@ -1,11 +1,15 @@
-export default function IconButton({ url, descritpion }) {
+export default function IconButton({ children, url, descritpion }) {
+  const cssClassButton = children
+    ? "cursor-pointer bg-transparent flex items-center gap-[0.5rem] hover:bg-[#F6F6F6] p-[0.5rem] rounded-[0.25rem]"
+    : "cursor-pointer bg-transparent";
+  const cssClassImg = children
+    ? ""
+    : "hover:bg-[#F6F6F6] p-[0.5rem] rounded-[0.25rem]";
+
   return (
-    <button className="cursor-pointer bg-transparent">
-      <img
-        className="hover:bg-[#F6F6F6] p-[0.5rem] rounded-[0.25rem]"
-        src={url}
-        alt={descritpion}
-      />
+    <button className={cssClassButton}>
+      <img className={cssClassImg} src={url} alt={descritpion} />
+      {children}
     </button>
   );
 }
