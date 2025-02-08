f276f38cf1c4fa5ed9b62210de5fd9b13b918be7
RYham <rafyahamza99@gmail.com>
2025-02-09 00:18:08 +0100

    Clean up the data

--- src/layout/ProductsLayout.jsx
+++ src/layout/ProductsLayout.jsx
@@ -2,13 +2,13 @@ import { Link, Outlet, useParams, useRouteLoaderData } from "react-router-dom";
 import AsyncLoader from "../components/AsyncLoader";
 
 const ProductsLayout = () => {
-  const urlParams = useParams();
+  //   const urlParams = useParams();
 
-  const { product } = useRouteLoaderData("product-detail");
+  //   const { product } = useRouteLoaderData("product-detail");
 
   //   const pathObj = ["Catalog", "Brand"];
 
-  console.log(product);
+  //   console.log(product);
 
   return (
     <>
@@ -16,11 +16,11 @@ const ProductsLayout = () => {
         <li>
           <Link to="/">Home</Link>
         </li>
-        <AsyncLoader promise={product}>
+        {/* <AsyncLoader promise={product}>
           {(loadedProduct) => {
             console.log(loadedProduct);
           }}
-        </AsyncLoader>
+        </AsyncLoader> */}
 
         {/* {pathObj.map((path, index) => (
           <li key={index}>
