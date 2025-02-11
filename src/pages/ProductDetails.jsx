7a0ba7dcc5d7fc68c9d8b3060c3abb00a3e52846
RYham <rafyahamza99@gmail.com>
2025-02-11 01:08:26 +0100

    Creation of the Dropdown component

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,16 +1,18 @@
-import { useLoaderData, useParams, useSearchParams } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import { sendHttpRequest } from "../hooks/useHttp";
 import AsyncLoader from "../components/AsyncLoader";
 import ProductItem from "../components/UI/ProductItem";
 
 const ProductDetails = () => {
   const { product } = useLoaderData();
+
   return (
     <>
       <h1>Products details</h1>
       <ul className="grid grid-cols-6 grid-flow-row gap-[1rem]">
         <AsyncLoader promise={product}>
           {(loadedProduct) => {
+            console.log(loadedProduct);
             return (
               <ProductItem
                 key={loadedProduct.id}
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,27 +1,54 @@
-import { useLoaderData, useNavigate, useParams } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import { sendHttpRequest } from "../hooks/useHttp";
 import AsyncLoader from "../components/AsyncLoader";
 import ProductItem from "../components/UI/ProductItem";
 
+import downArrowImg from "../assets/downArrow.svg";
+import upArrowImg from "../assets/upArrow.svg";
+
 export default function ProductsPage() {
   const { products } = useLoaderData();
-  const { category } = useParams();
   const navigate = useNavigate();
 
   return (
-    <>
-      <ul className="grid grid-cols-6 grid-flow-row gap-[1rem]">
+    <section className="flex gap-[2rem] px-[10rem] py-[1.5rem]">
+      <ul className="flex flex-col gap-[1.5rem] w-full max-w-[16rem]">
+        <div className="flex justify-between py-[0.75rem] border-b-1 border-[#B5B5B5]">
+          <p>Title</p>
+          <img src={downArrowImg} alt="Down Arrow image" />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-between text-[#A7A7A7]">
+            <p>From</p>
+            <p>To</p>
+          </div>
+          <div className="flex justify-between w-ful">
+            <input
+              className="max-w-[6.75rem] py-[0.5rem] px-[0.5rem] border-[1px] border-[#9F9F9F] rounded-sm"
+              type="number"
+              name=""
+              id=""
+            />
+            <input
+              className="max-w-[6.75rem] py-[0.5rem] px-[0.5rem] border-[1px] border-[#9F9F9F] rounded-sm"
+              type="number"
+              name=""
+              id=""
+            />
+          </div>
+        </div>
+      </ul>
+
+      <ul className="w-full grid grid-cols-3 grid-flow-row gap-[1rem]">
         <AsyncLoader promise={products}>
           {(loadedProducts) => {
-            const filteredProducts = loadedProducts?.filter(
-              (product) => product.category === category
-            );
-
-            if (filteredProducts.length <= 0) {
+            console.log(loadedProducts);
+            if (loadedProducts.length <= 0) {
               return <p>There is no product for this category</p>;
             }
 
-            return filteredProducts?.map((product) => (
+            return loadedProducts?.map((product) => (
               <ProductItem
                 key={product.id}
                 name={product.name}
@@ -37,7 +64,7 @@ export default function ProductsPage() {
           }}
         </AsyncLoader>
       </ul>
-    </>
+    </section>
   );
 }
 
