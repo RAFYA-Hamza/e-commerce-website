5d9fe3e79f7fe5cebb4254b6bd64114e6744dfae
RYham <rafyahamza99@gmail.com>
2025-02-20 23:58:56 +0100

    Styles of the content

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -8,6 +8,10 @@ import Details from "../components/Details";
 const ProductDetails = () => {
   const { product } = useLoaderData();
 
+  function handleClick() {
+    console.log("clicked");
+  }
+
   return (
     <AsyncLoader promise={product}>
       {(loadedProduct) => {
@@ -15,23 +19,36 @@ const ProductDetails = () => {
         return (
           <section className=" flex flex-col">
             <div className="px-[10rem] py-[7rem] flex items-center gap-[3rem]">
-              <div className="">
+              <div className="flex-1">
                 <img
                   src={`http://localhost:8080/${loadedProduct.image}`}
                   alt="The images"
                 />
               </div>
-              <div className="flex flex-col gap-[2rem]">
+
+              <div className="flex flex-col gap-[2rem] flex-1">
                 <div className="flex flex-col gap-[1rem]">
                   <div className="flex flex-col gap-[1.5rem]">
-                    <h1>Apple iPhone 14 Pro Max</h1>
-                    <p>$1399</p>
+                    <h1 className="text-[2.5rem] leading-none font-bold">
+                      Apple iPhone 14 Pro Max
+                    </h1>
+                    <p className="text-[2rem] font-medium">$1399</p>
                   </div>
 
                   <form className="flex flex-col gap-[1.5rem]">
                     <div className="flex">
-                      <p>Select color:</p>
-                      <input type="radio" />
+                      <p className="text-[#0C0C0C]">Select color:</p>
+                      <div className="relative h-[2rem] w-[2rem]">
+                        <input
+                          onClick={(e) => handleClick(e.target.value)}
+                          className="w-full h-full z-2"
+                          type="radio"
+                        />
+                        <label
+                          className="w-full h-full absolute left-[0%] rounded-[100%] bg-transparent"
+                          htmlFor=""
+                        ></label>
+                      </div>
                     </div>
 
                     <div className="flex gap-[1rem]">
@@ -40,6 +57,11 @@ const ProductDetails = () => {
                       <Tabs />
                     </div>
                     <div className="w-full grid grid-cols-3 grid-flow-row gap-[1rem]">
+                      <Details />
+                      <Details />
+                      <Details />
+                      <Details />
+                      <Details />
                       <Details />
                     </div>
                   </form>
